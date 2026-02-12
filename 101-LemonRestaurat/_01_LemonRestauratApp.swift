46f9894428de5b5ce3a544555b9df262ee1b4a74
ashley randle <randlea67@gmail.com>
2026-02-11 21:01:12 -0500

    Initial Commit

---
+++ 101-LemonRestaurat/_01_LemonRestauratApp.swift
@@ -0,0 +1,17 @@
+//
+//  _01_LemonRestauratApp.swift
+//  101-LemonRestaurat
+//
+//  Created by Ashley Randle on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct _01_LemonRestauratApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
