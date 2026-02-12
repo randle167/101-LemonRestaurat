a7de0c12450c07832ec6c7a72f00bbd2ca614b08
ashley randle <randlea67@gmail.com>
2026-02-12 12:51:41 -0500

    little lemon update

---
+++ 101-LemonRestaurat/practice/Variables.playground/Sources/ReservationForm.swift
@@ -0,0 +1,18 @@
+//
+//  ReservationForm.swift
+//  
+//
+//  Created by Ashley Randle on 2/11/26.
+//
+
+import SwiftUI
+
+struct ReservationForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReservationForm()
+}
